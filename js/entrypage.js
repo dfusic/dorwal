10594231030204efb722fd154f30d4c1c8de29d5
SM-durofusic <d.fusic@searchmetrics.com>
2020-05-03 13:05:23 +0200

    New images in the gallery, introduction text

--- js/entrypage.js
+++ js/entrypage.js
@@ -1,26 +1,28 @@
-$(document).ready(function() {
-  $('#croatian').hover(function(){
-    $('#title').text('Ljepota Kreativnosti');
-    $('#videoInstructionsLink').text('Video upute');
-    $('#copyrightLink').text('          Prava pristupa & zahvale');
-    $('#copyrightLink').attr('href','copyright/croatian.html')
+$(document).ready(function () {
+  $("#croatian").hover(function () {
+    $("#title").text("Ljepota Kreativnosti");
+    $("#videoInstructionsLink").text("Video upute");
+    $("#copyrightLink").text("          Prava pristupa & zahvale");
+    $("#copyrightLink").attr("href", "copyright/croatian.html");
   });
-  $('#english').hover(function(){
-    $('#title').text('Beauty of Creativity');
-    $('#videoInstructionsLink').text('Video instructions');
-    $('#copyrightLink').text('Copyright & special thanks');
-    $('#copyrightLink').attr('href','copyright/english.html')
+  $("#english").hover(function () {
+    $("#title").text("Beauty of Creativity");
+    $("#videoInstructionsLink").text("Video instructions");
+    $("#copyrightLink").text("Copyright & special thanks");
+    $("#copyrightLink").attr("href", "copyright/english.html");
+    $(".predgovorLink").text("Introduction");
+    $(".predgovorLink").attr("href", "../intro.html");
   });
-  $('#german').hover(function(){
-    $('#title').text('Die schönheit der kreativität');
-    $('#videoInstructionsLink').text('Videoanweisungen');
-    $('#copyrightLink').text('Urheberrecht & besonderen dank');
-    $('#copyrightLink').attr('href','copyright/german.html')
+  $("#german").hover(function () {
+    $("#title").text("Die schönheit der kreativität");
+    $("#videoInstructionsLink").text("Videoanweisungen");
+    $("#copyrightLink").text("Urheberrecht & besonderen dank");
+    $("#copyrightLink").attr("href", "copyright/german.html");
   });
-  $('#french').hover(function(){
-    $('#title').text('La Beaute de la creativite');
-    $('#videoInstructionsLink').text('Vidéo instructions')
-    $('#copyrightLink').text('droits d\'auteur & remerciement spécial');
-    $('#copyrightLink').attr('href','copyright/french.html')
+  $("#french").hover(function () {
+    $("#title").text("La Beaute de la creativite");
+    $("#videoInstructionsLink").text("Vidéo instructions");
+    $("#copyrightLink").text("droits d'auteur & remerciement spécial");
+    $("#copyrightLink").attr("href", "copyright/french.html");
   });
 });
